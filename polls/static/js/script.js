ba7aaa58369a3ce8593f88f541866534c4a3a5b1
rajendrarrv <rajendrarajaramv@gmail.com>
2023-02-16 19:01:31 +0530

    add registration model

--- polls/static/js/script.js
+++ polls/static/js/script.js
@@ -1,8 +1,9 @@
 var modal = document.getElementById('id01');
+var modal1 = document.getElementById('id02');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal || modal1) {
         modal.style.display = "none";
     }
 }
