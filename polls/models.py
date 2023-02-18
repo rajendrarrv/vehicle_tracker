f59d607d008a5b8b2021c6d781720632469aed26
rajendrarrv <rajendrarajaramv@gmail.com>
2023-02-18 12:56:40 +0530

    Created a user table

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name_text = models.CharField(max_length=20)
+    email_text = models.CharField(max_length=20, unique=True)
+    mobile_no_text = models.CharField(max_length=12, unique=True)
+    pub_date = models.DateTimeField('date published')
+    password_text = models.CharField(max_length=20);
+
+    def __str__(self):
+        return self.name_text
--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,10 @@ def home(request):
 
 
 def registration(request):
+    name = request.POST.get("name", "")
+    email = request.POST.get("email", "")
+    mobile_no = request.POST.get("mobile_no", "")
+    print("Name " + name + "\nEmail " + email + "\nMobile no " + mobile_no)
     return HttpResponse("this is registration page", content_type='text/plain')
 
 
