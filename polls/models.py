7d3ce2bb2d879226b398a9e2897b7742a1e01dc2
rajendrarrv <rajendrarajaramv@gmail.com>
2023-02-19 10:19:02 +0530

    update the users table

--- polls/models.py
+++ polls/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name_text = models.CharField(max_length=20)
-    email_text = models.CharField(max_length=20, unique=True)
-    mobile_no_text = models.CharField(max_length=12, unique=True)
+    email_text = models.CharField(max_length=50, unique=True, null=True)
+    mobile_no_text = models.CharField(max_length=12, unique=True, null=True)
+    password_text = models.CharField(max_length=10, null=True)
     pub_date = models.DateTimeField('date published')
-    password_text = models.CharField(max_length=20);
 
     def __str__(self):
         return self.name_text
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
+from time import timezone
+
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.template.context_processors import csrf
+from polls.models import User
+from django.utils import timezone
 
 
 def home(request):
@@ -11,7 +14,10 @@ def registration(request):
     name = request.POST.get("name", "")
     email = request.POST.get("email", "")
     mobile_no = request.POST.get("mobile_no", "")
-    print("Name " + name + "\nEmail " + email + "\nMobile no " + mobile_no)
+    password = request.POST.get("password", "")
+    user = User(name_text=name, email_text=email, mobile_no_text=mobile_no, password_text=password,
+                pub_date=timezone.now())
+    user.save()
     return HttpResponse("this is registration page", content_type='text/plain')
 
 
