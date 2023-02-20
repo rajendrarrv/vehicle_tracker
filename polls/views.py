9aa333ffaa5e33c9b29b1b3ee20aadfb95c83d19
rajendrarrv <rajendrarajaramv@gmail.com>
2023-02-20 18:52:55 +0530

    Registration is completed

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from sqlite3 import IntegrityError
 from time import timezone
 
 from django.http.response import HttpResponse
@@ -11,14 +12,25 @@ def home(request):
 
 
 def registration(request):
+    msg = "this is registration page"
     name = request.POST.get("name", "")
     email = request.POST.get("email", "")
     mobile_no = request.POST.get("mobile_no", "")
     password = request.POST.get("password", "")
-    user = User(name_text=name, email_text=email, mobile_no_text=mobile_no, password_text=password,
-                pub_date=timezone.now())
-    user.save()
-    return HttpResponse("this is registration page", content_type='text/plain')
+    confirm_password = request.POST.get("confirm_password", "")
+
+    if password != confirm_password:
+        msg = "Please confirm the password properly"
+        return HttpResponse(msg, content_type='text/plain')
+
+    try:
+        user = User(name_text=name, email_text=email, mobile_no_text=mobile_no, password_text=password,
+                    pub_date=timezone.now())
+        user.save()
+    except Exception as e:
+        msg = "user is already exist"
+        return HttpResponse(msg + str(e), content_type='text/plain')
+    return HttpResponse(msg, content_type='text/plain')
 
 
 def login(request):
