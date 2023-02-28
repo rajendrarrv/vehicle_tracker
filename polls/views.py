7031cc41f51d0e56fb14463c5712ed0eea30a0fd
rajendrarrv <rajendrarajaramv@gmail.com>
2023-02-28 13:49:44 +0530

    create signup and registered APIS

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from time import timezone
 
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from sqlalchemy import null
 
 from polls.models import User
@@ -11,30 +12,30 @@ from django.utils import timezone
 
 
 def home(request):
-    if check_session(request):
-        return HttpResponseRedirect('dashboard')
+    # if check_session(request):
+    #     return HttpResponseRedirect('dashboard')
     return render(request, 'polls/index.html')
 
 
+@csrf_exempt
 def registration(request):
-    msg = "this is registration page"
     name = request.POST.get("name", "")
     email = request.POST.get("email", "")
     mobile_no = request.POST.get("mobile_no", "")
     password = request.POST.get("password", "")
-    confirm_password = request.POST.get("confirm_password", "")
-
-    if password != confirm_password:
-        msg = "Please confirm the password properly"
-        return HttpResponse(msg, content_type='text/plain')
 
     try:
         user = User(name_text=name, email_text=email, mobile_no_text=mobile_no, password_text=password,
                     pub_date=timezone.now())
         user.save()
+
+        msg = {"data": "record is saved successfully", "error": {}}
+        HttpResponse(str(msg), content_type='text/plain')
     except Exception as e:
         msg = "user is already exist"
-        return HttpResponse(msg + str(e), content_type='text/plain')
+        msg = {"data": {}, "error": e}
+
+        return HttpResponse(str(msg), content_type='text/plain')
     return HttpResponse(msg, content_type='text/plain')
 
 
@@ -43,19 +44,21 @@ def message(request, title, message):
     return render(request, 'polls/message.html', context)
 
 
+@csrf_exempt
 def login(request):
     username = request.POST.get("username", "")
     password = request.POST.get("password", "")
     query_set = User.objects.filter(email_text=username).filter(password_text=password)
-    if len(query_set) == 0:
 
-        return message(request, title="Error", message="user does not exist")
+    if not query_set.exists():
+        # The Queryset is empty ...
+        msg = {"error": "something is going wrong"}
+        return HttpResponse(str(msg), content_type='text/plain')
     else:
+        # The Queryset has results ...
         user = query_set[0]
-        request.session['email'] = user.email_text
-        request.session['name'] = user.name_text
-        request.session['mobile'] = user.mobile_no_text
-        return HttpResponseRedirect('dashboard')
+        msg = {"data": {"email": user.email_text, "name": user.name_text, "mobile": user.mobile_no_text}, "error": {}}
+    return HttpResponse(str(msg), content_type='text/plain')
 
 
 def dashboard(request):
